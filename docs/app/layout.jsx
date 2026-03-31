476b566b16d3471b4831e75b32602f9e17456281
PopcornDev1 <e.coiley@icloud.com>
2026-03-31 19:38:15 +1300

    feat: add Vercel Analytics and Speed Insights to docs

--- docs/app/layout.jsx
+++ docs/app/layout.jsx
@@ -1,6 +1,8 @@
 import { Layout, Navbar } from 'nextra-theme-docs'
 import { Head } from 'nextra/components'
 import { getPageMap } from 'nextra/page-map'
+import { Analytics } from '@vercel/analytics/react'
+import { SpeedInsights } from '@vercel/speed-insights/next'
 import 'nextra-theme-docs/style.css'
 
 export const metadata = {
@@ -115,6 +117,8 @@ export default async function RootLayout({ children }) {
         >
           {children}
         </Layout>
+        <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   )
